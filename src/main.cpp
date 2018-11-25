0a207fb026659a81ed92830b0bc30b2caa6b3972
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-11-25 13:04:40 -0600

    Added color corection
    and i'm not sure why

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "FastLED.h"
 #define NUM_LEDS_PER_STRIP 20
 #define NUM_STRIPS 8
+#define LED_TYPE WS2812
 
 int startButtonPin = 2;
 int startbuttonState = HIGH;
@@ -13,26 +14,15 @@ long debounce = 200;
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 void setup(){
-  FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
 }
 
 void loop(){
-  static uint8_t hue = 0;
-  for(int i = 0; i < NUM_STRIPS; i++) {
-    for(int j = 0; j < NUM_LEDS_PER_STRIP; j++) {
-      leds[(i*NUM_LEDS_PER_STRIP) + j] = CHSV((32*i) + hue+j,192,255);
-    }
-  }
-
-  // Set the first n leds on each strip to show which strip it is
   for(int i = 0; i < NUM_STRIPS; i++) {
     for(int j = 0; j <= i; j++) {
       leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB::Red;
     }
   }
 
-  hue++;
-
-  LEDS.show();
-  LEDS.delay(10);
+  FastLED.show();
 }
