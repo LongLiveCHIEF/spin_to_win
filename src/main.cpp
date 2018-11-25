57eb005df7ee4c420169d0657fdd6b93cbc1e24e
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-11-25 10:03:05 -0600

    setup LED controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "FastLED.h"
-#define LEDS_PER_STRIP 20;
+#define NUM_LEDS_PER_STRIP 20;
+#define NUM_STRIPS 8;
 
 int startButtonPin = 2;
 int startbuttonState = HIGH;
@@ -9,10 +10,10 @@ int startButtonPrevious = LOW;
 long lastStartButtonPushTime = 0;
 
 long debounce = 200;
-CRGB leds[NUM_LEDS_PER_STRIP]:
+CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 void setup(){
-
+  FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
 }
 
 void loop(){
