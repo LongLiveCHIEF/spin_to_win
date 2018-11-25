f1249d6bd999cc2bdf5bbefc6a7142291aad2b42
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-11-25 11:43:33 -0600

    fix compile and platformio upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "FastLED.h"
-#define NUM_LEDS_PER_STRIP 20;
-#define NUM_STRIPS 8;
+#define NUM_LEDS_PER_STRIP 20
+#define NUM_STRIPS 8
 
 int startButtonPin = 2;
 int startbuttonState = HIGH;
@@ -17,5 +17,22 @@ void setup(){
 }
 
 void loop(){
+  static uint8_t hue = 0;
+  for(int i = 0; i < NUM_STRIPS; i++) {
+    for(int j = 0; j < NUM_LEDS_PER_STRIP; j++) {
+      leds[(i*NUM_LEDS_PER_STRIP) + j] = CHSV((32*i) + hue+j,192,255);
+    }
+  }
 
+  // Set the first n leds on each strip to show which strip it is
+  for(int i = 0; i < NUM_STRIPS; i++) {
+    for(int j = 0; j <= i; j++) {
+      leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB::Red;
+    }
+  }
+
+  hue++;
+
+  LEDS.show();
+  LEDS.delay(10);
 }
