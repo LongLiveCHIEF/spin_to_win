b6c42fa33acc090cec1c24967e915449081ca3c9
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-11-26 08:26:56 -0600

    platform updates

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,16 @@ CRGBArray<NUM_STRIPS * NUM_LEDS_PER_STRIP> leds;
 
 
 void setup(){
-  Serial.begin(9600);
-  LEDS.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  LEDS.clearData();
-  Serial.println("----Starting Spin to Win-----");
+  FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
 }
 
 int selectorRangeStart = 0;
-int selectorRangeEnd = (NUM_LEDS_PER_STRIP*NUM_SELECTOR_STRIPS)-1;
+int selectorRangeEnd = selectorRangeStart + SELECTOR_WIDTH -1;
 int wheelRangeStart = (NUM_LEDS_PER_STRIP*NUM_SELECTOR_STRIPS);
 int wheelRangeEnd = (NUM_STRIPS*NUM_LEDS_PER_STRIP);
 
 void loop() {
   leds(selectorRangeStart,selectorRangeEnd).fill_solid(CRGB::Blue);
   leds(wheelRangeStart, wheelRangeEnd).fill_solid(CRGB::Red);
-  LEDS.show();
+  FastLED.show();
 }
