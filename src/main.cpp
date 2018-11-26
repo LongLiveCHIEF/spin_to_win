a27a532d7c2ba98dc0da17c6fc95e9e188862e89
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-11-25 20:08:03 -0600

    switch to using CRGBSets

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "Arduino.h"
 #include "FastLED.h"
 #define NUM_LEDS_PER_STRIP 20
+#define NUM_SELECTOR_STRIPS 2
+#define NUM_WHEEL_STRIPS 6
 #define NUM_STRIPS 8
 #define LED_TYPE WS2812
 
+int spaceWidth = 8;
 int startButton = 24;
 int stopButton = 25;
 int startbuttonState = HIGH;
@@ -12,43 +15,14 @@ int startButtonPrevious = LOW;
 long lastStartButtonPushTime = 0;
 
 long debounce = 200;
-CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
+CRGBArray<NUM_STRIPS * NUM_LEDS_PER_STRIP> leds;
 
-void fadeall() { for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) { leds[i].nscale8(250); } }
-
-void id_strips(){
-  for(int i = 0; i < NUM_STRIPS; i++) {
-    for(int j = 0; j <= i; j++) {
-      stripIdentifier[(i*NUM_LEDS_PER_STRIP) + j] = CRGB::Red;
-    }
-  }
-
-  FastLED.show();
-}
 
 void setup(){
   Serial.begin(9600);
   FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.clearData();
   Serial.println("----Starting Spin to Win-----");
 }
 
-void loop(){
-  static uint8_t hue = 0;
-  for(int i = 0; i < NUM_STRIPS; i++) {
-    for(int j = 0; j < NUM_LEDS_PER_STRIP; j++) {
-      leds[j] = CHSV(hue++, 255, 255);
-    }
-    FastLED.show();
-    fadeall();
-    delay(10);
-  }
-
-  for(int i = 0; i < NUM_STRIPS; i++) {
-    for(int j = (NUM_LEDS_PER_STRIP) -1; j >= 0; j--) {
-      leds[j] = CHSV(hue++, 255, 255);
-    }
-    FastLED.show();
-    fadeall();
-    delay(10);
-  }
-}
+void loop() { static uint8_t hue=0; leds.fill_rainbow(hue++); FastLED.delay(30); }
