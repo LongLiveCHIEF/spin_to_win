255f63dcc8faf9df7b98f8ee8b310ad00151336f
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-11-26 10:14:40 -0600

    add start button

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@
 #define SELECTOR_WIDTH 3
 #define SPACE_WIDTH 8
 
-int startButton = 24;
-int stopButton = 25;
-int startbuttonState = HIGH;
+bool spinning = false;
+int startButton = 7;
+int stopButton = 6;
+int startButtonState = HIGH;
 int startButtonReading;
 int startButtonPrevious = LOW;
 long lastStartButtonPushTime = 0;
@@ -18,18 +19,60 @@ long lastStartButtonPushTime = 0;
 long debounce = 200;
 CRGBArray<NUM_STRIPS * NUM_LEDS_PER_STRIP> leds;
 
+void selector(){
+  while(startButtonState == HIGH){
+    for(int i = 0; i < (NUM_LEDS_PER_STRIP-SELECTOR_WIDTH); i++) {
+      leds(i,i+SELECTOR_WIDTH-1).fill_solid(CRGB::Blue);
+      leds(i+1,i+SELECTOR_WIDTH-1+1).fill_solid(CRGB::Blue);
+      FastLED.show();
+      FastLED.delay(10);
+      leds(i,i+SELECTOR_WIDTH-1).fill_solid(CRGB::Black);
+      leds(i+1,i+SELECTOR_WIDTH-1+1).fill_solid(CRGB::Black);
+      FastLED.show();
+      FastLED.delay(10);
+    }
+
+    for(int i =(NUM_LEDS_PER_STRIP-SELECTOR_WIDTH-1); i >= SELECTOR_WIDTH; i--){
+      leds(i,i+SELECTOR_WIDTH-1).fill_solid(CRGB::Blue);
+      leds(i-1,i+SELECTOR_WIDTH-1-1).fill_solid(CRGB::Blue);
+      FastLED.show();
+      FastLED.delay(10);
+      leds(i,i+SELECTOR_WIDTH-1).fill_solid(CRGB::Black);
+      leds(i-1,i+SELECTOR_WIDTH-1-1).fill_solid(CRGB::Black);
+      FastLED.show();
+      FastLED.delay(10);
+    }
+  }
+}
+
 
 void setup(){
+  Serial.begin(115200);
+  pinMode(startButton, INPUT);
   FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  Serial.println("Begin Serial Output");
 }
 
 int selectorRangeStart = 0;
 int selectorRangeEnd = selectorRangeStart + SELECTOR_WIDTH -1;
 int wheelRangeStart = (NUM_LEDS_PER_STRIP*NUM_SELECTOR_STRIPS);
 int wheelRangeEnd = (NUM_STRIPS*NUM_LEDS_PER_STRIP);
+int bottomSelectorOffset = NUM_STRIPS-1;
 
 void loop() {
-  leds(selectorRangeStart,selectorRangeEnd).fill_solid(CRGB::Blue);
-  leds(wheelRangeStart, wheelRangeEnd).fill_solid(CRGB::Red);
-  FastLED.show();
+  startButtonReading = digitalRead(startButton);
+  if (startButtonReading == HIGH && startButtonPrevious == LOW && millis() - lastStartButtonPushTime > debounce){
+    Serial.println("Start Button Pushed");
+    if (startButtonState == HIGH) {
+      startButtonState = LOW;
+      spinning = false;
+    } else {
+      startButtonState = HIGH;
+      spinning = true;
+    }
+
+    lastStartButtonPushTime = millis();
+  }
+  startButtonPrevious = startButtonReading;
+  selector();
 }
